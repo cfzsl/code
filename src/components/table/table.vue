1776dfdcd814d5b5f595ffaa8666368301aedff1
my-code <2438853928@qq.com>
2019-10-14 23:54:22 +0800

    分页及其样式

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -1,33 +1,108 @@
 <template>
-<!-- 列表 -->
+  <!-- 列表 -->
   <div id="table">
-    <el-table :data="tableData" border style="width: 1128px">
-      <el-table-column class='text' prop="name" label="姓名" width="159px"></el-table-column>
-      <el-table-column class='text' prop="province" label="区域" width="159px"></el-table-column>
-      <el-table-column class='text' prop="city" label="单位" width="159px"></el-table-column>
-      <el-table-column class='text' prop="date" label="入职时间" width="113px"></el-table-column>
-      <el-table-column class='text' prop="updata" label="离职时间" width="123px"></el-table-column>
-      <el-table-column class='text' prop="zipiphone" label="联系方式" width="123px"></el-table-column>
-      <el-table-column class='text' prop="msg" label="是否超龄" width="140px"></el-table-column>
-      <el-table-column class='text' fixed="right" label="操作" width="142px">
+    <el-table
+      :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)"
+      border
+      style="width: 1128px"
+    >
+      <el-table-column
+        class="text"
+        prop="name"
+        label="姓名"
+        width="159px"
+      ></el-table-column>
+      <el-table-column
+        class="text"
+        prop="province"
+        label="区域"
+        width="159px"
+      ></el-table-column>
+      <el-table-column
+        class="text"
+        prop="city"
+        label="单位"
+        width="159px"
+      ></el-table-column>
+      <el-table-column
+        class="text"
+        prop="date"
+        label="入职时间"
+        width="113px"
+      ></el-table-column>
+      <el-table-column
+        class="text"
+        prop="updata"
+        label="离职时间"
+        width="123px"
+      ></el-table-column>
+      <el-table-column
+        class="text"
+        prop="zipiphone"
+        label="联系方式"
+        width="123px"
+      ></el-table-column>
+      <el-table-column
+        class="text"
+        prop="msg"
+        label="是否超龄"
+        width="140px"
+      ></el-table-column>
+      <el-table-column class="text" fixed="right" label="操作" width="142px">
         <template slot-scope="scope">
-          <el-button class='tableButton1' type="button" size="small"  @click="tableDate = true">详情</el-button>
-          <el-button class='tableButton2' type="button" size="small">删除</el-button>
+          <el-button
+            class="tableButton1"
+            type="button"
+            size="small"
+            @click="tableDate = true"
+            >详情</el-button
+          >
+          <el-button
+            class="tableButton2"
+            type="button"
+            @click="deletList"
+            size="small"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      class="paginationList"
+      background
+      layout="total, prev, pager, next"
+      :total="tableData.length"
+      @current-change="handleCurrentChange"
+      @size-change="handleSizeChange"
+    >
+    </el-pagination>
+
     <!-- 弹框 -->
-    <el-dialog title="添加人员信息" :visible.sync="tableDate" width="426px" class='dialogText'>
+    <el-dialog
+      title="添加人员信息"
+      :visible.sync="tableDate"
+      width="426px"
+      class="dialogText"
+    >
       <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <div class="formTop">
-          <el-form-item label="区域" class='formTopSelected'>
-            <el-select v-model="formInline.region" placeholder="活动区域" class="selectTop">
+          <el-form-item label="区域" class="formTopSelected">
+            <el-select
+              v-model="formInline.region"
+              placeholder="活动区域"
+              class="selectTop"
+            >
               <el-option label="东营区" value="shanghai"></el-option>
               <el-option label="滨州区" value="beijing"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="单位">
-            <el-select v-model="formInline.relog" placeholder="活动单位" class="selectTop">
+            <el-select
+              v-model="formInline.relog"
+              placeholder="活动单位"
+              class="selectTop"
+            >
               <el-option label="环卫一部" value="shanghai"></el-option>
               <el-option label="环卫二部" value="beijing"></el-option>
             </el-select>
@@ -46,7 +121,11 @@
           <el-input v-model="formInline.mobile"></el-input>
         </el-form-item>
         <el-form-item label="状态">
-          <el-select v-model="formInline.stated" placeholder="状态" class='selectBot'>
+          <el-select
+            v-model="formInline.stated"
+            placeholder="状态"
+            class="selectBot"
+          >
             <el-option label="在职" value="shanghai"></el-option>
             <el-option label="离职" value="beijing"></el-option>
           </el-select>
@@ -58,20 +137,30 @@
           <el-input v-model="formInline.updatetime"></el-input>
         </el-form-item>
         <el-form-item label="负责道路名称">
-          <el-select v-model="formInline.region" placeholder="负责道路名称" class='selectBot'>
+          <el-select
+            v-model="formInline.region"
+            placeholder="负责道路名称"
+            class="selectBot"
+          >
             <el-option label="滨湖大道" value="shanghai"></el-option>
             <el-option label="翠湖公园路" value="beijing"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="负责区域范围">
-          <el-select v-model="formInline.region" placeholder="负责区域范围" class='selectBot'>
+          <el-select
+            v-model="formInline.region"
+            placeholder="负责区域范围"
+            class="selectBot"
+          >
             <el-option label="东营翠湖公园" value="shanghai"></el-option>
             <el-option label="陶然公园" value="beijing"></el-option>
           </el-select>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="tableDate = false" class='formButon'>保存</el-button>
+        <el-button type="primary" @click="tableDate = false" class="formButon"
+          >保存</el-button
+        >
       </span>
     </el-dialog>
   </div>
@@ -79,32 +168,12 @@
 
 <script>
 export default {
-  methods: {
-
-  },
+  methods: {},
   data() {
     return {
+      pagesize: 10,
+      currpage: 1,
       tableData: [
-        {
-          name: "李旦",
-          province: "东营区",
-          city: "环卫一部",
-          address: "上海市普陀区金沙江路 1518 弄",
-          date: "2016-05-02",
-          updata: "在职",
-          zipiphone: "15927227885",
-          msg: "否"
-        },
-        {
-          name: "李旦",
-          province: "东营区",
-          city: "环卫一部",
-          address: "上海市普陀区金沙江路 1518 弄",
-          date: "2016-05-02",
-          updata: "在职",
-          zipiphone: "15927227885",
-          msg: "否"
-        },
         {
           name: "李旦",
           province: "东营区",
@@ -116,30 +185,59 @@ export default {
           msg: "否"
         }
       ],
-      tableDate:false,
-        formInline: {
+      tableDate: false,
+      formInline: {
         user: "",
         region: "",
         relog: ""
-      },
+      }
     };
+  },
+  created() {
+    this.getlist();
+  },
+  methods: {
+    getlist() {
+      for (let i = 0; i < 99; i++) {
+        this.tableData.push({
+          name: "李旦",
+          province: "东营区",
+          city: "环卫一部",
+          address: "上海市普陀区金沙江路 1518 弄",
+          date: "2016-05-02",
+          updata: "在职",
+          zipiphone: "15927227885",
+          msg: "否"
+        });
+      }
+    },
+    deletList() {
+      console.log("删除指定项");
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
   }
 };
 </script>
 
 <style rel="stylesheet/scss" lang="scss" type="text/scss" scoped>
-.tableButton1,.tableButton2 {
-    float: left;
-    width: 55px;
-    height: 25px;
-    padding: 0;
-    margin: 0;
+.tableButton1,
+.tableButton2 {
+  float: left;
+  width: 55px;
+  height: 25px;
+  padding: 0;
+  margin: 0;
 }
 .tableButton1 {
-    margin-right: 10px;
+  margin-right: 10px;
 }
 .dialogText {
-    text-align: center;
+  text-align: center;
 }
 .demo-form-inline {
   text-align: right;
@@ -157,31 +255,25 @@ export default {
     }
   }
   .el-form-item {
-      margin-bottom: 2px;
-      .el-input {
-          width: 240px;
-          height: 32px;
-      }
-  }
-  .selectBot {
+    margin-bottom: 2px;
+    .el-input {
       width: 240px;
       height: 32px;
+    }
+  }
+  .selectBot {
+    width: 240px;
+    height: 32px;
   }
 }
 .formButon {
-    width: 127px;
-    height: 40px;
-    text-align: center;
+  width: 127px;
+  height: 40px;
+  text-align: center;
+}
+.paginationList {
+  float: right;
+  margin-top: 32px;
+  padding: 0;
 }
 </style>
-
-
-
-
-
-
-
-
-
-
-
--- src/views/base/base.vue
+++ src/views/base/base.vue
@@ -5,16 +5,28 @@
       <div class="searchTop">
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
           <el-form-item label="姓名">
-            <el-input class="searchInput" v-model="formInline.user" placeholder="姓名"></el-input>
+            <el-input
+              class="searchInput"
+              v-model="formInline.user"
+              placeholder="姓名"
+            ></el-input>
           </el-form-item>
           <el-form-item label="区域">
-            <el-select class="searchInput" v-model="formInline.region" placeholder="活动区域">
+            <el-select
+              class="searchInput"
+              v-model="formInline.region"
+              placeholder="活动区域"
+            >
               <el-option label="东营区" value="shanghai"></el-option>
               <el-option label="滨州区" value="beijing"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="活动单位">
-            <el-select class="searchInput" v-model="formInline.relog" placeholder="活动单位">
+            <el-select
+              class="searchInput"
+              v-model="formInline.relog"
+              placeholder="活动单位"
+            >
               <el-option label="环卫一部" value="shanghai"></el-option>
               <el-option label="环卫二部" value="beijing"></el-option>
             </el-select>
@@ -26,24 +38,39 @@
       </div>
       <!-- 按钮 -->
       <div class="searchBot">
-        <el-button class="buttonBot" @click="dialogVisible = true">添加人员信息</el-button>
+        <el-button class="buttonBot" @click="dialogVisible = true"
+          >添加人员信息</el-button
+        >
         <el-button class="buttonBot">导入模板下载</el-button>
         <el-button class="buttonBot">人员信息导入</el-button>
         <el-button class="buttonBotLast">导出全员信息</el-button>
       </div>
     </div>
     <!-- 弹窗 -->
-    <el-dialog title="添加人员信息" :visible.sync="dialogVisible" width="426px" class='dialogText'>
+    <el-dialog
+      title="添加人员信息"
+      :visible.sync="dialogVisible"
+      width="426px"
+      class="dialogText"
+    >
       <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <div class="formTop">
-          <el-form-item label="区域" class='formTopSelected'>
-            <el-select v-model="formInline.region" placeholder="活动区域" class="selectTop">
+          <el-form-item label="区域" class="formTopSelected">
+            <el-select
+              v-model="formInline.region"
+              placeholder="活动区域"
+              class="selectTop"
+            >
               <el-option label="东营区" value="shanghai"></el-option>
               <el-option label="滨州区" value="beijing"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="单位">
-            <el-select v-model="formInline.relog" placeholder="活动单位" class="selectTop">
+            <el-select
+              v-model="formInline.relog"
+              placeholder="活动单位"
+              class="selectTop"
+            >
               <el-option label="环卫一部" value="shanghai"></el-option>
               <el-option label="环卫二部" value="beijing"></el-option>
             </el-select>
@@ -62,7 +89,11 @@
           <el-input v-model="formInline.mobile"></el-input>
         </el-form-item>
         <el-form-item label="状态">
-          <el-select v-model="formInline.stated" placeholder="状态" class='selectBot'>
+          <el-select
+            v-model="formInline.stated"
+            placeholder="状态"
+            class="selectBot"
+          >
             <el-option label="在职" value="shanghai"></el-option>
             <el-option label="离职" value="beijing"></el-option>
           </el-select>
@@ -74,28 +105,38 @@
           <el-input v-model="formInline.updatetime"></el-input>
         </el-form-item>
         <el-form-item label="负责道路名称">
-          <el-select v-model="formInline.region" placeholder="负责道路名称" class='selectBot'>
+          <el-select
+            v-model="formInline.region"
+            placeholder="负责道路名称"
+            class="selectBot"
+          >
             <el-option label="滨湖大道" value="shanghai"></el-option>
             <el-option label="翠湖公园路" value="beijing"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="负责区域范围">
-          <el-select v-model="formInline.region" placeholder="负责区域范围" class='selectBot'>
+          <el-select
+            v-model="formInline.region"
+            placeholder="负责区域范围"
+            class="selectBot"
+          >
             <el-option label="东营翠湖公园" value="shanghai"></el-option>
             <el-option label="陶然公园" value="beijing"></el-option>
           </el-select>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogVisible = false" class='formButon'>保存</el-button>
+        <el-button
+          type="primary"
+          @click="dialogVisible = false"
+          class="formButon"
+          >保存</el-button
+        >
       </span>
     </el-dialog>
     <!-- 表格 -->
     <div class="table">
       <Table></Table>
-    </div>
-    <div class="pagination">
-      <el-pagination background :page-size="100" layout="prev, pager, next" :total="900"></el-pagination>
     </div>
   </div>
 </template>
@@ -115,7 +156,7 @@ export default {
   },
   methods: {
     onSubmit() {
-      console.log("submit!");
+      console.log("查啥?");
     }
   },
   components: {
@@ -130,8 +171,8 @@ export default {
   height: 76px;
   margin-top: 16px;
   .searchTop {
-      float: left;
-      margin-bottom: 16px;
+    float: left;
+    margin-bottom: 16px;
   }
   .searchBot {
     float: right;
@@ -156,7 +197,7 @@ export default {
   margin-top: 16px;
 }
 .dialogText {
-    text-align: center;
+  text-align: center;
 }
 .demo-form-inline {
   text-align: right;
@@ -174,21 +215,21 @@ export default {
     }
   }
   .el-form-item {
-      margin-bottom: 2px;
-      .el-input {
-          width: 240px;
-          height: 32px;
-      }
-  }
-  .selectBot {
+    margin-bottom: 2px;
+    .el-input {
       width: 240px;
       height: 32px;
+    }
+  }
+  .selectBot {
+    width: 240px;
+    height: 32px;
   }
 }
 .formButon {
-    width: 127px;
-    height: 40px;
-    text-align: center;
+  width: 127px;
+  height: 40px;
+  text-align: center;
 }
 .pagination {
   float: right;
